065120f30f927d12474b234bd822ac6deed894f6
alexey-polyashov <polyashofff@yandex.ru>
2021-03-18 00:52:16 +0300

    урок 3_1

---
+++ Box.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Box<T extends Fruit> {
+
+    private List<T> content = new ArrayList<>();
+
+    public double getWeight(){
+        float summ = 0;
+        for (T fr: content) {
+            summ += fr.getWeight();
+        }
+        return summ;
+    }
+
+    public boolean compare(Box<?> o) {
+        if(this.getWeight()==o.getWeight()){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public void pour(Box<T> dst){
+        for (T fr: content) {
+            dst.add(fr);
+        }
+        content.clear();
+    }
+
+    public void add(T fr){
+        content.add(fr);
+    }
+}
